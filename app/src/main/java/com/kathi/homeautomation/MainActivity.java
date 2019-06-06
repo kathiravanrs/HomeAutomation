f89713d88797f5594e6658bcb731e3a7686c467a
kathiravan <kathiravanrs@gmail.com>
2019-06-06 09:57:15 +0530

    Initial commit

---
+++ app/src/main/java/com/kathi/homeautomation/MainActivity.java
@@ -0,0 +1,337 @@
+package com.kathi.homeautomation;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+
+import com.microsoft.azure.sdk.iot.device.DeviceClient;
+import com.microsoft.azure.sdk.iot.device.DeviceTwin.DeviceMethodData;
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeReason;
+import com.microsoft.azure.sdk.iot.device.IotHubEventCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubMessageResult;
+import com.microsoft.azure.sdk.iot.device.IotHubStatusCode;
+import com.microsoft.azure.sdk.iot.device.Message;
+import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String connString = "HostName=kathi-home-automation.azure-devices.net;" +
+            "DeviceId=Honor10;" +
+            "SharedAccessKey=7usiv8TGKeW0qHS+2FrkFFA4UjF/r0W0Cxjyaz+iR40=";
+
+    private double temperature;
+    private double humidity;
+    private String msgStr;
+    private Message sendMessage;
+    private String lastException;
+
+    private DeviceClient client;
+
+    IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+
+
+    boolean green;
+    boolean pink;
+    boolean blue;
+    boolean hall;
+    boolean kitchen;
+
+    private int msgSentCount = 0;
+    private int receiptsConfirmedCount = 0;
+    private int sendFailuresCount = 0;
+    private int msgReceivedCount = 0;
+    private int sendMessagesInterval = 5000;
+
+    private final Handler handler = new Handler();
+    private Thread sendThread;
+
+    private static final int METHOD_SUCCESS = 200;
+    public static final int METHOD_THROWS = 403;
+    private static final int METHOD_NOT_DEFINED = 404;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        Switch greenRoom = findViewById(R.id.greenRoomSwitch);
+        greenRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                green = isChecked;
+                start();
+            }
+        });
+
+        Switch pinkRoom = findViewById(R.id.pinkRoomSwitch);
+        pinkRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                pink = isChecked;
+                start();
+
+            }
+        });
+
+        Switch hallRoom = findViewById(R.id.hallSwitch);
+        hallRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                hall = isChecked;
+                start();
+            }
+
+        });
+
+        Switch blueRoom = findViewById(R.id.blueRoomSwitch);
+        blueRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                blue = isChecked;
+                start();
+
+            }
+        });
+
+        Switch kitchenRoom = findViewById(R.id.kitchenSwitch);
+        kitchenRoom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+               kitchen = isChecked;
+                start();
+
+            }
+        });
+
+    }
+
+    private void start()
+    {
+        sendThread = new Thread(new Runnable() {
+            public void run()
+            {
+                try
+                {
+                    initClient();
+                        sendMessages();
+                }
+                catch (Exception e)
+                {
+                    lastException = "Exception while opening IoTHub connection: " + e;
+                    handler.post(exceptionRunnable);
+                }
+            }
+        });
+
+        sendThread.start();
+    }
+
+
+    final Runnable exceptionRunnable = new Runnable() {
+        public void run() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(lastException);
+            builder.show();
+            System.out.println(lastException);
+
+        }
+    };
+
+    final Runnable methodNotificationRunnable = new Runnable() {
+        public void run() {
+            Context context = getApplicationContext();
+            CharSequence text = "Set Send Messages Interval to " + sendMessagesInterval + "ms";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    };
+
+    private void sendMessages()
+    {
+        msgStr = "{\"Kitchen\":" + kitchen + ", \"Green Room\":" + green + ", \"Hall\":" + hall
+                + ", \"Blue Room\":" + blue + ", \"Pink Room\":" + pink+"}";
+        try
+        {
+//            Handler handler = new Handler();
+//            handler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//
+//                }
+//            }, 2000);
+            sendMessage = new Message(msgStr);
+            sendMessage.setMessageId(java.util.UUID.randomUUID().toString());
+            System.out.println("Message Sent: " + msgStr);
+            EventCallback eventCallback = new EventCallback();
+            client.sendEventAsync(sendMessage, eventCallback, msgSentCount);
+            msgSentCount++;
+
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception while sending event: " + e);
+        }
+    }
+
+    private void initClient() throws URISyntaxException, IOException
+    {
+        client = new DeviceClient(connString, protocol);
+
+        try
+        {
+            client.registerConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallbackLogger(), new Object());
+            client.open();
+            MessageCallback callback = new MessageCallback();
+            client.setMessageCallback(callback, null);
+            client.subscribeToDeviceMethod(new SampleDeviceMethodCallback(), getApplicationContext(), new DeviceMethodStatusCallBack(), null);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception while opening IoTHub connection: " + e);
+            client.closeNow();
+            System.out.println("Shutting down...");
+        }
+    }
+
+    class EventCallback implements IotHubEventCallback
+    {
+        public void execute(IotHubStatusCode status, Object context)
+        {
+            Integer i = context instanceof Integer ? (Integer) context : 0;
+            System.out.println("IoT Hub responded to message " + i.toString()
+                    + " with status " + status.name());
+
+            if((status == IotHubStatusCode.OK) || (status == IotHubStatusCode.OK_EMPTY))
+            {
+                receiptsConfirmedCount++;
+            }
+            else
+            {
+                sendFailuresCount++;
+            }
+        }
+    }
+
+    class MessageCallback implements com.microsoft.azure.sdk.iot.device.MessageCallback
+    {
+        public IotHubMessageResult execute(Message msg, Object context)
+        {
+            System.out.println(
+                    "Received message with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET));
+            msgReceivedCount++;
+            return IotHubMessageResult.COMPLETE;
+        }
+    }
+
+    protected static class IotHubConnectionStatusChangeCallbackLogger implements IotHubConnectionStatusChangeCallback
+    {
+        @Override
+        public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason statusChangeReason, Throwable throwable, Object callbackContext)
+        {
+            System.out.println();
+            System.out.println("CONNECTION STATUS UPDATE: " + status);
+            System.out.println("CONNECTION STATUS REASON: " + statusChangeReason);
+            System.out.println("CONNECTION STATUS THROWABLE: " + (throwable == null ? "null" : throwable.getMessage()));
+            System.out.println();
+
+            if (throwable != null)
+            {
+                throwable.printStackTrace();
+            }
+
+            if (status == IotHubConnectionStatus.DISCONNECTED)
+            {
+                //connection was lost, and is not being re-established. Look at provided exception for
+                // how to resolve this issue. Cannot send messages until this issue is resolved, and you manually
+                // re-open the device client
+            }
+            else if (status == IotHubConnectionStatus.DISCONNECTED_RETRYING)
+            {
+                //connection was lost, but is being re-established. Can still send messages, but they won't
+                // be sent until the connection is re-established
+            }
+            else if (status == IotHubConnectionStatus.CONNECTED)
+            {
+                //Connection was successfully re-established. Can send messages.
+            }
+        }
+    }
+
+    private int method_setSendMessagesInterval(Object methodData) throws UnsupportedEncodingException, JSONException
+    {
+        String payload = new String((byte[])methodData, "UTF-8").replace("\"", "");
+        JSONObject obj = new JSONObject(payload);
+        sendMessagesInterval = obj.getInt("sendInterval");
+        handler.post(methodNotificationRunnable);
+        return METHOD_SUCCESS;
+    }
+
+    private int method_default(Object data)
+    {
+        System.out.println("invoking default method for this device");
+        // Insert device specific code here
+        return METHOD_NOT_DEFINED;
+    }
+
+    protected class DeviceMethodStatusCallBack implements IotHubEventCallback
+    {
+        public void execute(IotHubStatusCode status, Object context)
+        {
+            System.out.println("IoT Hub responded to device method operation with status " + status.name());
+        }
+    }
+
+    protected class SampleDeviceMethodCallback implements com.microsoft.azure.sdk.iot.device.DeviceTwin.DeviceMethodCallback
+    {
+        @Override
+        public DeviceMethodData call(String methodName, Object methodData, Object context)
+        {
+            DeviceMethodData deviceMethodData ;
+            try {
+                switch (methodName) {
+                    case "setSendMessagesInterval": {
+                        int status = method_setSendMessagesInterval(methodData);
+                        deviceMethodData = new DeviceMethodData(status, "executed " + methodName);
+                        break;
+                    }
+                    default: {
+                        int status = method_default(methodData);
+                        deviceMethodData = new DeviceMethodData(status, "executed " + methodName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                int status = METHOD_THROWS;
+                deviceMethodData = new DeviceMethodData(status, "Method Throws " + methodName);
+            }
+            return deviceMethodData;
+        }
+    }
+}
